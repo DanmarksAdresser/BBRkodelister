e8ffefccae158dccb7af3a913a2f7fc165530712
finnjordal <finn@jordal.dk>
2019-09-13 11:24:24 +0200

    Første bud på kodegenerering

--- index.js
+++ index.js
@@ -1,27 +1,66 @@
 // Inspireret af https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/
 //
+const url = require('url');
 const rp = require('request-promise');
 const $ = require('cheerio');
-const url = 'https://teknik.bbr.dk/kodelister';
+const kodelisterurl = 'https://teknik.bbr.dk/kodelister';
 
-async function main(url) {
+async function main() {
 	//debugger;
-	let html= await rp(url);
+	let html= await rp(kodelisterurl);
 	//console.log(html);
 	let kodelister= $('div > select > option', html);
-	console.log(kodelister.length);
+	//console.log('Antal kodelister: ' + kodelister.length);
 	//console.log(kodelister);
 	for (let i= 1; i<kodelister.length; i++) {
-		let kurl= kodelister[i].attribs.value; 
-		console.log(kurl);
-		let khtml= await rp(kurl);		
+		let kurl= url.parse(kodelister[i].attribs.value);
+		let names= kurl.pathname.split('/');
+		//console.log(names[names.length-1]);
+		startkode(names[names.length-1]);
+		let khtml= await rp(kodelister[i].attribs.value);		
 		let kodeliste= $('ul.kodeliste-list > li', khtml);
-		console.log(kodeliste.length);
+		//console.log(kodeliste.length);
+		//let kodeognavn= /^([\d\w]+)\s-\s([\S+])$/;
+		let kodeognavn= /^([\d\w]+)\s-\s([\s\w\(\)æøåÆØÅé,.<>\-\/&§”:+…­­­­­­­½\[\]\–]+)$/;
 		for (let j= 0; j<kodeliste.length; j++) {
-			console.log(kodeliste[j].children[0].data);
+			let result= kodeognavn.exec(kodeliste[j].children[0].data);
+			if (result) {
+				casekode(result[1], result[2]);
+				// console.log(result[1], + ': ' + result[2]);
+			}
+			else {				
+				console.log(kodeliste[j].children[0].data);
+			}
 		}
+		slutkode();
 	}
 
 }
 
-main(url);
+function startkode(kodelistenavn) {
+	console.log('');
+	console.log('function get' + kodelistenavn + '(kode) {');
+	console.log('\tswitch (kode) { ');
+}
+
+function casekode(kode, navn) {
+	let tal= /^\d+$/;
+	if (tal.exec(kode)) {		
+		console.log('\tcase ' + kode + ':');
+	}	
+	else {		
+		console.log('\tcase "' + kode + '":');
+	}
+	console.log('\t\tnavn= "' + navn + '";');	
+	console.log('\t\tbreak;');	
+}
+
+function slutkode() {	
+	console.log('\tdefault:');	
+	console.log('\t\tnavn= "Ukendt kode";');	
+	console.log('\t}');			
+	console.log('\treturn navn;');		
+	console.log('}');	
+}
+
+main();
